f0825ef0f7e6210b3298e8b1087c06d0f6fa19f5
Hounaida202 <etrichih@gmail.com>
2025-10-27 17:13:51 +0100

    JRA-123

---
+++ Smartl/src/main/java/com/smartlogi/smartl/SmartlApplication.java
@@ -0,0 +1,13 @@
+package com.smartlogi.smartl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartlApplication.class, args);
+    }
+
+}
---
+++ Smartl/src/test/java/com/smartlogi/smartl/SmartlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartlogi.smartl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
